6965e31d9bd22ca8fe8ccfe5450170ff59a2a7d2
Ben Ehmke <benehmke@gmail.com>
2021-09-09 11:20:36 -0500

    add Postgres setup to server-bootstrap

--- server-bootstrap.sh
+++ server-bootstrap.sh
@@ -240,3 +240,12 @@ EOF
 
 sudo systemctl enable certbot.timer
 sudo systemctl start certbot.timer
+
+
+## For Postgres
+pacman -S postgresql
+sudo su - postgres
+initdb --locale en_US.UTF-8 -D /var/lib/postgres/data
+exit
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
